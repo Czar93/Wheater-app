1dcb2707c0611362bba4af35d68ccbb7390466c4
Adam Czarnecki <aczarnecki93@gmail.com>
2020-03-11 21:18:13 +0100

    Updates

---
+++ precache-manifest.92c3fe9f84258b474405b6436229b1a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1758dd1ee102414aa43b4f77bf813d",
+    "url": "/Wheater-app/index.html"
+  },
+  {
+    "revision": "3f17f5038e9392c071c2",
+    "url": "/Wheater-app/static/css/main.6111b159.chunk.css"
+  },
+  {
+    "revision": "9cd0d948f6dd811a62ab",
+    "url": "/Wheater-app/static/js/2.1a24de09.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Wheater-app/static/js/2.1a24de09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f17f5038e9392c071c2",
+    "url": "/Wheater-app/static/js/main.632f7f83.chunk.js"
+  },
+  {
+    "revision": "d5ae0abdcd8195c46704",
+    "url": "/Wheater-app/static/js/runtime-main.75c50a51.js"
+  }
+]);
